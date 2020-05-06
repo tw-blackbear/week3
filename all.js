370f77c2fcc8853bd56ee723cce4d7fa9ffe8ae0
tw-blackbear <46948504+tw-blackbear@users.noreply.github.com>
2020-05-06 20:57:39 +0800

    Add files via upload

---
+++ all.js
@@ -0,0 +1,8 @@
+
+    $(document).ready(function () {
+        $(".menuBtn").click(function (e) {
+          e.preventDefault();
+          $(".menu").toggleClass("active");
+        });
+      });
+          
